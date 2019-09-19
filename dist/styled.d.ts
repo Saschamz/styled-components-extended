6a04c645b2c6b3ee797d324d26bd035353c3ff2b
Sascha Ringström <sascha.ringstrom@redmind.se>
2019-09-19 15:22:11 +0200

    auto imports

--- dist/styled.d.ts
+++ dist/styled.d.ts
@@ -1,33 +1,4 @@
-export declare type TextKitProps = {
-    centered?: boolean;
-    capitalize?: boolean;
-    uppercase?: boolean;
-    lowercase?: boolean;
-    verticalMargin?: number;
-    fontWeight?: number;
-    fontSize?: number;
-};
-export declare type ViewKitProps = {
-    verticalMargin?: number;
-    horizontalMargin?: number;
-    marginTop?: number;
-    marginBottom?: number;
-    marginLeft?: number;
-    marginRight?: number;
-};
-export declare type FlexRowProps = {
-    spaceBetween?: boolean;
-    spaceAround?: boolean;
-    spaceEvenly?: boolean;
-    centered?: boolean;
-};
-export declare type CircleViewProps = {
-    size: number;
-    color?: string;
-};
-export declare type WhitespaceProps = {
-    space: number;
-};
+import { TextKitProps, ViewKitProps, FlexRowProps, CircleViewProps } from './types';
 export declare let Text: import("styled-components").StyledComponent<typeof import("react-native").Text, import("styled-components").DefaultTheme, TextKitProps, never>;
 export declare function addBaseCssToText(css: any): void;
 export declare let View: import("styled-components").StyledComponent<typeof import("react-native").View, import("styled-components").DefaultTheme, ViewKitProps, never>;
@@ -36,7 +7,7 @@ export declare const CenteredView: import("styled-components").StyledComponent<t
 export declare const CenteredFillView: import("styled-components").StyledComponent<typeof import("react-native").View, any, ViewKitProps, never>;
 export declare const FlexRow: import("styled-components").StyledComponent<typeof import("react-native").View, any, ViewKitProps & FlexRowProps, never>;
 export declare const AlignEndView: import("styled-components").StyledComponent<typeof import("react-native").View, import("styled-components").DefaultTheme, {}, never>;
-export declare const Whitespace: import("styled-components").StyledComponent<typeof import("react-native").View, any, ViewKitProps & WhitespaceProps, never>;
+export declare const Whitespace: import("styled-components").StyledComponent<typeof import("react-native").View, any, ViewKitProps, never>;
 export declare const CircleView: import("styled-components").StyledComponent<typeof import("react-native").View, any, ViewKitProps & CircleViewProps, never>;
 export declare const BottomAbsoluteContainer: import("styled-components").StyledComponent<typeof import("react-native").View, any, ViewKitProps, never>;
 export declare const TopAbsoluteContainer: import("styled-components").StyledComponent<typeof import("react-native").View, any, ViewKitProps, never>;
--- src/styled.ts
+++ src/styled.ts
@@ -1,41 +1,12 @@
 import styled from 'styled-components/native'
 
 import * as mixins from './mixins'
-
-export type TextKitProps = {
-  centered?: boolean
-  capitalize?: boolean
-  uppercase?: boolean
-  lowercase?: boolean
-  verticalMargin?: number
-  fontWeight?: number
-  fontSize?: number
-}
-
-export type ViewKitProps = {
-  verticalMargin?: number
-  horizontalMargin?: number
-  marginTop?: number
-  marginBottom?: number
-  marginLeft?: number
-  marginRight?: number
-}
-
-export type FlexRowProps = {
-  spaceBetween?: boolean
-  spaceAround?: boolean
-  spaceEvenly?: boolean
-  centered?: boolean
-}
-
-export type CircleViewProps = {
-  size: number
-  color?: string
-}
-
-export type WhitespaceProps = {
-  space: number
-}
+import {
+  TextKitProps,
+  ViewKitProps,
+  FlexRowProps,
+  CircleViewProps
+} from './types'
 
 export let Text = styled.Text`
   ${(props: TextKitProps) => props.centered && `text-align: center`}
